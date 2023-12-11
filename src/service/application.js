aeffa32ae487fa6f8ebbb7e21d3f8a20997f7e6a
David-Vandensteen <dvandensteen@gmail.com>
2023-12-11 14:50:57 +0100

    Update

--- src/service/application.js
+++ src/service/application.js
@@ -4,15 +4,23 @@ const { terminal } = terminalKit;
 
 const channel = () => {
   terminal.red('Enter channel\n');
-  terminal.inputField((err, channelInput) => {
+  terminal.inputField((err, channelInput) => channelInput);
+};
 
-  });
+const controller = () => {
+  terminal.red('Enter controller\n');
+  terminal.inputField((err, controllerInput) => controllerInput);
 };
 
 const command = () => {
+  let channelInput = null;
+  let controllerInput = null;
   terminal.red('Enter command\n');
   terminal.inputField((err, commandInput) => {
-    if (commandInput === 'c') channel();
+    if (commandInput === 'c') channelInput = channel();
+    if (commandInput === 'cc') controllerInput = controller();
+    terminal.green('Channel', channelInput, '\n');
+    terminal.green('Controller', controllerInput, '\n');
     process.exit();
   });
 };
